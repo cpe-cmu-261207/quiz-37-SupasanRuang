58bb90aa69b19e44ba77260b21e1f92abecdb1b1
SupasanRuang <supasan_ruang@cmu.ac.th>
2022-09-15 14:33:22 +0700

    update

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -3,11 +3,42 @@ import Reply from "./Reply";
 
 export default function Comment(props) {
   return (
-    <div>
-      {/* Entire Comment div */}
-      <div></div>
+    <>
+      <div className="d-flex gap-2 my-2" key={props.key}>
+        <img
+          src={props.image}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{props.username}</span>
+          <br />
+          <span>{props.text}</span>
 
-      {/*render Reply here... */}
-    </div>
+          <div className="d-flex align-items-center gap-1">
+            {props.like > 0 && (
+              <>
+                <img src="/like.svg" width={20}></img>
+                <span className="text-muted"> {props.like} คน</span>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      {props.replies.map((x, i) => (
+        <Reply
+          username={x.username}
+          key={i}
+          text={x.replyText}
+          like={x.likeNum}
+          image={x.userImagePath}
+        />
+      ))}
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,26 @@ export default function Home() {
         style={{ maxWidth: "700px" }}
         className="mx-auto bg-white p-3 rounded rounded-3 shadow-sm"
       >
+        <PostOwner
+          name={"Supasan Ruangchutipopan 640610671"}
+          image={"/profileImages/Thinking_Face_Emoji_large.png"}
+          title={"Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207"}
+          like={555}
+        />
         {/* PostOwner Example*/}
-        <div className="vstack gap-3">
+
+        {/* <div className="vstack gap-3">
           <div className="d-flex align-items-center gap-3">
             <img
-              src="/profileImages/handsome.jpg"
+              src="/profileImages/Thinking_Face_Emoji_large.png"
               width="48"
               height="48"
               className="rounded-circle"
               style={{ objectFit: "cover" }}
             />
-            <span className="fw-semibold fs-5">Chayanin Suatap 12345678</span>
+            <span className="fw-semibold fs-5">
+              Supasan Ruangchutipopan 640610671
+            </span>
           </div>
           <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
           <div className="d-flex align-items-center gap-1">
@@ -31,10 +40,31 @@ export default function Home() {
             <span className="text-muted">100 คน</span>
           </div>
           <hr className="m-0" />
-        </div>
+        </div> */}
 
         {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
+        {/* <Comment
+          username={55}
+          key={5}
+          text={5}
+          like={5}
+          replies={5}
+          image={"/profileImages/Thinking_Face_Emoji_large.png"}
+        /> */}
+        {comments.map((x, i) => (
+          <>
+            {/* <span>5555</span> */}
+            <Comment
+              username={x.username}
+              key={i}
+              text={x.commentText}
+              like={x.likeNum}
+              replies={x.replies}
+              image={x.userImagePath}
+            />
+          </>
+        ))}
+        {/* <div className="d-flex gap-2 my-2">
           <img
             src="/profileImages/lisa.jpg"
             width="48"
@@ -54,26 +84,9 @@ export default function Home() {
               <span className="text-muted">999 คน</span>
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#E5E7EB" }}
-          >
-            <span className="fw-semibold">หมาน้อย</span>
-            <br />
-            <span>จริงค้าบบบบบบบบ</span>
-          </div>
-        </div>
       </div>
     </div>
   );
